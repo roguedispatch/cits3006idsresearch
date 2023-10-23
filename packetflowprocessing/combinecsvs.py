4a51569a3dbaa8a452e295b871d342fce26be2d6
roguedispatch <56345778+roguedispatch@users.noreply.github.com>
2023-10-23 19:37:08 +0800

    Update combinecsvs.py

--- packetflowprocessing/combinecsvs.py
+++ packetflowprocessing/combinecsvs.py
@@ -6,6 +6,31 @@ def read_headers(filename):
         headers = next(reader)
     return headers
 
+def get_top_level_protocol(protocol_stack):
+    protocols = [
+        "udp", "arp", "tcp", "ospf", "icmp", "igmp", "sctp", "udt", "sep", "sun-nd", 
+        "swipe", "mobile", "pim", "rtp", "ipnip", "ip", "ggp", "st2", "egp", "cbt", 
+        "emcon", "nvp", "igp", "xnet", "argus", "bbn-rcc", "chaos", "pup", "hmp", 
+        "mux", "dcn", "prm", "trunk-1", "xns-idp", "trunk-2", "leaf-1", "leaf-2", 
+        "irtp", "rdp", "iso-tp4", "netblt", "mfe-nsp", "merit-inp", "3pc", "xtp", 
+        "idpr", "tp++", "ddp", "idpr-cmtp", "ipv6", "il", "idrp", "ipv6-frag", 
+        "sdrp", "ipv6-route", "gre", "rsvp", "mhrp", "bna", "esp", "i-nlsp", "narp", 
+        "ipv6-no", "tlsp", "skip", "ipv6-opts", "any", "cftp", "sat-expak", "kryptolan", 
+        "rvd", "ippc", "sat-mon", "ipcv", "visa", "cpnx", "cphb", "wsn", "pvp", 
+        "br-sat-mon", "wb-mon", "wb-expak", "iso-ip", "secure-vmtp", "vmtp", "vines", 
+        "ttp", "nsfnet-igp", "dgp", "tcf", "eigrp", "sprite-rpc", "larp", "mtp", "ax.25", 
+        "ipip", "micp", "aes-sp3-d", "encap", "etherip", "pri-enc", "gmtp", "pnni", 
+        "ifmp", "aris", "qnx", "a/n", "scps", "snp", "ipcomp", "compaq-peer", "ipx-n-ip", 
+        "vrrp", "zero", "pgm", "iatp", "ddx", "l2tp", "srp", "stp", "smp", "uti", 
+        "sm", "ptp", "fire", "crtp", "isis", "crudp", "sccopmce", "sps", "pipe", 
+        "iplt", "unas", "fc", "ib"
+    ]
+    for protocol in reversed(protocol_stack.split(':')):
+        if protocol in protocols:
+            return protocol
+
+    return protocol_stack.split(':')[-1]
+
 def combine_tsvs(start, end, output_file):
     first_file = f"{start}.pcap.tsv"
     original_headers = read_headers(first_file)
@@ -15,6 +40,8 @@ def combine_tsvs(start, end, output_file):
         output_headers.remove('sll.src.eth')
     if 'eth.src' not in output_headers:
         output_headers.append('eth.src')
+    if 'frame.protocols' in original_headers: 
+        output_headers[original_headers.index('frame.protocols')] = 'protocol' 
 
     with open(output_file, 'w', newline='') as out_file:
         writer = csv.writer(out_file, delimiter='\t')
@@ -31,15 +58,18 @@ def combine_tsvs(start, end, output_file):
                 reader = csv.DictReader(file, delimiter='\t')
 
                 for row in reader:
-                    # Transfer sll.src.eth to eth.src and remove sll.src.eth
                     if 'sll.src.eth' in row:
                         row['eth.src'] = row['sll.src.eth']
                         del row['sll.src.eth']
+
+                    if 'frame.protocols' in row:
+                        row['protocol'] = get_top_level_protocol(row['frame.protocols'])
+                        del row['frame.protocols']
 
                     writer.writerow([row[key] for key in output_headers])
 
         print(f"Combined TSV written to {output_file}")
 
-start, end = 1, 27
-output_file = 'UNSWFull.tsv'
+start, end = 1, 5
+output_file = 'UNSWFirstFive.tsv'
 combine_tsvs(start, end, output_file)
