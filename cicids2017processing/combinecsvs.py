fcd9407cd9d2c7d25dc40b927eeb3c929da5e3cd
LarryHH <larryhieuhuynh@gmail.com>
2023-11-02 15:03:36 +0800

    handle arbitrary number of files

--- cicids2017processing/combinecsvs.py
+++ cicids2017processing/combinecsvs.py
@@ -1,53 +1,51 @@
 import csv
 
+
 def read_headers(filename):
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         reader = csv.reader(f)
         headers = next(reader)
     return headers
 
+
 def is_empty(row):
-    return all(cell.strip() == '' for cell in row)
+    return all(cell.strip() == "" for cell in row)
 
-def combine_csvs(file1, file2, output_file):
-    headers1 = read_headers(file1)
-    headers2 = read_headers(file2)
 
-    if headers1 == headers2:
-        with open(output_file, 'w', newline='') as out_file:
+def combine_csv_files(files, output_file):
+    # Read headers from all files and ensure they match
+    headers_list = [read_headers(file) for file in files]
+    headers_set_list = [set(headers) for headers in headers_list]
+    common_headers = set.intersection(*headers_set_list)
+
+    if all(headers == headers_list[0] for headers in headers_list):
+        with open(output_file, "w", newline="") as out_file:
             writer = csv.writer(out_file)
+            writer.writerow(headers_list[0])  # Write the common headers
 
-            writer.writerow(headers1)
-
-            with open(file1, 'r') as f1, open(file2, 'r') as f2:
-                reader1 = csv.reader(f1)
-                reader2 = csv.reader(f2)
-
-                next(reader1)
-                next(reader2)
-
-                for row in reader1:
-                    if not is_empty(row):
-                        writer.writerow(row)
-
-                for row in reader2:
-                    if not is_empty(row):
-                        writer.writerow(row)
+            for file in files:
+                with open(file, "r") as f:
+                    reader = csv.reader(f)
+                    next(reader)  # Skip the header row
+                    for row in reader:
+                        if not is_empty(row):
+                            writer.writerow(row)
 
         print(f"Combined CSV written to {output_file}")
-
     else:
-        set1, set2 = set(headers1), set(headers2)
-        common = set1.intersection(set2)
-        only_in_file1 = set1 - common
-        only_in_file2 = set2 - common
+        # For files with headers not matching the common set, print the unique headers
+        for i, file in enumerate(files):
+            unique_headers = headers_set_list[i] - common_headers
+            if unique_headers:
+                print(f"Headers only in {file}: {', '.join(unique_headers)}")
 
-        if only_in_file1:
-            print(f"Headers only in {file1}: {', '.join(only_in_file1)}")
-        if only_in_file2:
-            print(f"Headers only in {file2}: {', '.join(only_in_file2)}")
 
-file1 = 'Thursday-WorkingHours-Morning-WebAttacks.pcap_ISCX.csv'
-file2 = 'Thursday-WorkingHours-Afternoon-Infilteration.pcap_ISCX.csv'
-output_file = 'Thursday-WorkingHours-Full.csv'
-combine_csvs(file1, file2, output_file)
+if __name__ == "__main__":
+    file1 = "datasets/MachineLearningCVE/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv"
+    file2 = (
+        "datasets/MachineLearningCVE/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv"
+    )
+    file3 = "datasets/MachineLearningCVE/Friday-WorkingHours-Morning.pcap_ISCX.csv"
+    output_file = "Friday-WorkingHours-Full.csv"
+
+    combine_csv_files([file1, file2, file3], output_file)
